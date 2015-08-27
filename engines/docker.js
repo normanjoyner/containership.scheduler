52bf43dfccfe0a4c27475e087d97f1b7b74f5d78
Norman Joyner <norman.joyner@gmail.com>
2015-08-27 00:14:30 -0400

    removes debugging

--- engines/docker.js
+++ engines/docker.js
@@ -57,7 +57,7 @@ module.exports = {
     // start container
     start: function(options){
         var self = this;
-        try{
+
         var node = this.core.cluster.legiond.get_attributes();
 
         var pre_pull_middleware = _.map(self.middleware.pre_pull, function(middleware, middleware_name){
@@ -115,7 +115,6 @@ module.exports = {
                 });
             }
         });
-}catch(e){console.log(e.stack)}
     },
 
     // stop container
