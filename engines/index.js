1e98190d0f244350ed92ab26e58c5147c131120c
Jeremy Kross <jeremy.kross@gmail.com>
2016-12-01 13:10:39 -0500

    Removed forever monitor, refactored docker engine
    * Removed forever monitor, refactored docker engine.
    
    * First cleanup pass.
    
    * More cleanup.
    
    * Added check on reconcile for appropriate host.
    
    * More cleanup.
    
    * Fixed misapplication of async logic.
    
    * More small cleanup - fixed a code-review regression.
    
    * Small update.

--- engines/index.js
+++ engines/index.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const DockerEngine = require('./docker-engine');
+
 module.exports = {
-    docker: require([__dirname, "docker"].join("/"))
+    docker: (core) => new DockerEngine(core)
 }
