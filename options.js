06b542eda130c3c245fb8ec14c7feddc0e73a34b
Ashley Schuett <ashley@containership.io>
2017-01-31 06:27:00 -0500

    add memory options for hosts

--- options.js
+++ options.js
@@ -4,6 +4,16 @@ module.exports = {
         help: "Interval on which container harmonization occurs",
         metavar: "INTERVAL",
         default: 15000
-    }
+    },
+
+    "scheduler-available-memory" : {
+        help: 'Amount of memory the containership scheduler can allocate to running containers (MiB)',
+        metavar: 'SCHEDULER_AVAILABLE_MEMORY'
+    },
+
+     "scheduler-available-cpus" : {
+         help: 'Number of CPUs the containership scheduler can allocate to running containers',
+         metavar: 'SCHEDULER_AVAILABLE_CPUS'
+     }
 
 }
